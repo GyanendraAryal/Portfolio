465ef0e816b1d6c0575e3af47c20c8262542e10d
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-20 22:05:02 +0545

    feat(ui): update App structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './Layouts/MainLayout';
+import AdminLayout from './Layouts/AdminLayout';
+import PublicRoutes from './routes/PublicRoutes';
+import AdminRoutes from './routes/AdminRoutes';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/*" element={<MainLayout />}>
+        <Route path="*" element={<PublicRoutes />} />
+      </Route>
+      <Route path="/admin/*" element={<AdminLayout />}>
+        <Route path="*" element={<AdminRoutes />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
