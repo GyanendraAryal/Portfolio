953b3a202ab056809ba2082190357942b607957b
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-21 21:41:37 +0545

    chore: update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-ui': ['framer-motion', 'lucide-react', 'clsx', 'tailwind-merge', 'react-hot-toast'],
+          'vendor-core': ['react', 'react-dom', 'react-router-dom', 'axios', 'zustand'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 })
