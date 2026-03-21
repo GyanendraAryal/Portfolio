bfe4176158b4ab9c311198cf5e40ae98b839e2ee
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-21 21:41:39 +0545

    refactor: update project routes

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import { getProjects, createProject, updateProject, deleteProject } from '../controllers/projectController.js';
 import { protect } from '../middlewares/authMiddleware.js';
+import upload from '../middlewares/uploadMiddleware.js';
 
 const router = express.Router();
 
 router.route('/')
   .get(getProjects)
   .post(protect, createProject);
+
+router.post('/upload', protect, upload.single('image'), (req, res) => {
+  res.json({
+    message: 'Image uploaded successfully',
+    url: req.file.path,
+  });
+});
 
 router.route('/:id')
   .put(protect, updateProject)
